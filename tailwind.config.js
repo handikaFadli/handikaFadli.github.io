e76ed87421a662acdd5639f76b345155313d23b4
handikaFadli <handikafadli23@gmail.com>
2023-03-10 22:54:23 +0700

    memperbaiki hero section dan about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,12 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#06b6d4",
+        primary: "#1a8fe3",
         dark: "#0f172a",
         grey: "#64748b",
+        coklat: "#ac6b34",
+        hitam: "#131419",
+        abu: "#1C1D24",
       },
       screens: {
         xl: "1120px",
